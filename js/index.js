e813b647e303eebaccc6b4a3c5f527a070866f83
MrZwqShuai <zwq1098621287@yahoo.com>
2016-08-12 23:21:37 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,95 @@
-ddd
+'use strict'
+var AudioFace = {
+
+}
+AudioFace.music = {
+    // var currentIndex=-1;
+    // //  播放器元素对象
+   
+    // // 歌曲列表
+    // var mlist=["music/youdiantian.mp3","music/tingmamadehua.mp3","music/qingtian.mp3"];
+    // // 图片路径
+    // var imglist=["blank.png","Lana Del Rey.jpg","0039Pp2W372JsM.jpg"];
+    // //歌曲路径
+    // var msrc=["music/youdiantian.mp3","music/tingmamadehua.mp3","music/qingtian.mp3"];
+    // //进度条
+    // var stick=document.getElementById("stick");
+
+}
+AudioFace.music.AudioM = function(Songname,URL,element,element1,element2) {
+    this.element = element;
+    this.element1 = element1;
+    this.element2 = element2;
+    this.name = Songname
+    this.init();
+    this.audioPlay();
+    this.audioControl(element);
+    this.audioPlay(element);
+    this.audioProcess();
+    console.log(this);
+};
+
+AudioFace.music.AudioM.prototype = {
+    constructor : AudioFace.music.AudioM,
+
+    init: function() {
+  
+        this.Audio = document.querySelector('#myAudio');
+     
+   
+    },
+
+    audioPlay:function(element,element1,element2){
+     
+          var _self = this;
+          
+           $(element).click(function(){
+            var songN = $(this).attr('name');
+            var singer = $(this).attr('singer')
+    var path = $(this).attr('path');
+        _self.Audio.src = path;
+        _self.Audio.play();
+        $('.xmpname').html(songN);
+        $('.xmpartist').html(singer)
+        $(element2).addClass(element1);
+
+      
+     })
+    },
+
+    audioControl: function(element,element1) {
+        var _self = this;
+        $(element).on('click',function(){
+            if(!_self.Audio.paused){
+              $(this).removeClass(element1);
+            _self.Audio.pause();
+            }else{
+              $(this).addClass(element1);
+                _self.Audio.play()
+            }
+        })
+    },
+    audioProcess:function(){
+         //   this.Audio.oncanplaythrough = function(){
+         //   var m = parseInt(this.duration/60);
+         //   var s = parseInt(this.duration%60);
+         //   m = m < 10 ? '0' + m : m;
+         //   s = s < 10 ? '0' + s : s;
+         //   $('#TotalTimer').html(m+':'+s)
+         //    };
+         //  this.Audio.ontimeupdate = function(){
+         //    var M = parseInt(this.currentTime/60);
+         //    var S = parseInt(this.currentTime%60);
+         //    M = M < 10 ? '0' + M : M;
+         //    S = S < 10 ? '0' + S : S;
+            
+         //    document.getElementById('TotalTimer').innerHTML = (M+':'+S)
+          
+         // }
+    },
+
+     audioNext:function(element){
+        
+    }
+
+}
