714203f5763172a2a229937f11ccf1388f976c5b
MrZwqShuai <zwq1098621287@yahoo.com>
2016-08-14 18:06:06 +0800

    Update Index1.js

--- js/Index1.js
+++ js/Index1.js
@@ -1,48 +1,16 @@
 !(function(document,window,$){
+		var arr = new Array();
  $('.Pageload').on('click',function(){
   $('.homepage').css({opacity:0,'z-index':-1})
 })
  // $('.icon-play').on('click',function(){
 
  // })
-    var num = 0,
-    $list = $('.container');
-    loadData($list,num)
-   function loadData($ele,num){
-   	    $.ajax({
-    	url:'json/music.json',
-         async:false,
-    	success:function(data){
-    		
-    		var str = ' ';
-    		$.each(data,function(i,item){
-    			
-                if(i==0){
-                    str+=[
-                    '<li class="musiclist" id="#musiclist" singer='+item.Singer+' path="'+item.songSrc+'" name="'+item.songName+'">'+'<span>'+("0"+(i+1))+'</span>'+'<div>'+item.songName+'<br>'+item.Singer+'</div></li>'
-                ].join('');
-            }
-                else if(i<10){
-                	 str+=[
-                    '<li class="musiclist" id="#musiclist" singer='+item.Singer+' path="'+item.songSrc+'" name="'+item.songName+'">'+'<span>'+("0"+(i+1))+'</span>'+'<div>'+item.songName+'<br>'+item.Singer+'</div> </li>'
-                ].join('');
-                }
-            
-            else{
-    			str+=[
-					'<li class="musiclist" id="#musiclist" singer='+item.Singer+' path="'+item.songSrc+'" name="'+item.songName+'">'+'<span>'+(i+1)+'</span>'+'<div>'+item.songName+'<br>'+item.Singer+'</div></li>'
-    			].join('');
-            }
-    		})
-    		
-    		$list.append(str);
-
-        
-    	},
-    	error:function(){
-    		alert('请求失败')
+   
+    $(window).on('mousemove',function(){
+    	if($(window).scrollTop()>=$(document).height()-$(window).height()){
+    		console.log("到底了")
     	}
-
     })
-   }
+
 })(document,window,Zepto)
