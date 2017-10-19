3dc4278ad52ddfbc1221fc54cebde77dd208db69
张文强 <zwq1098621287@yahoo.com>
2017-10-19 17:35:42 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -131,10 +131,10 @@ console.log('setShareInfo', setShareInfo)
 })();
 setShareInfo({
     title:          '温商贷红包',
-    summary:        '温商贷红包转前222',
+    summary:        '温商贷红包转前000',
     pic:            'http://qzonestyle.gtimg.cn/aoi/sola/20150617094556_OvfOpoRKRB.png',
-    url:            'https://mrzwqshuai.github.io/MyPhoneMusic/',
-    callback: function () {
+    share_url:            'https://mrzwqshuai.github.io/MyPhoneMusic/',
+    callback: function (result) {
 
     alert('result成功分享')
     alert(result)
