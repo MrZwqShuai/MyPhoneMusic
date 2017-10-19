98859e9bed269523f33993f606737cdca729466f
张文强 <zwq1098621287@yahoo.com>
2017-10-19 15:27:11 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,16 +5,7 @@ setShareInfo({
     title:          '父爱，在你看不到的地方',
     summary:        '父爱如山，感觉不到只因身在此山中',
     pic:            'http://qzonestyle.gtimg.cn/aoi/sola/20150617094556_OvfOpoRKRB.png',
-    url:            'http://qzs.qzone.qq.com//qzone/qzact/act/2015/father-day-m/index.html',
-    WXconfig:       {
-        swapTitleInWX: true,
-        appId: '',
-        timestamp: '',
-        nonceStr: '',
-        signature: ''
-    }
-},function (result) {
-    alert('result: 分享成功', result)
+    url:            'https://mrzwqshuai.github.io/MyPhoneMusic/'
 });
 var AudioFace = {}
 AudioFace.music = {}
