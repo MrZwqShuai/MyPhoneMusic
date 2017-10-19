554b0d4d410ab49ab717e3461da57baba1f44e35
张文强 <zwq1098621287@yahoo.com>
2017-10-19 16:03:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ setShareInfo({
     summary:        '父爱如山，感觉不到只因身在此山中',
     pic:            'http://qzonestyle.gtimg.cn/aoi/sola/20150617094556_OvfOpoRKRB.png',
     url:            'https://mrzwqshuai.github.io/MyPhoneMusic/'
+}, function(result) {
+    alert('result成功分享')
+    alert(result)
 });
 var AudioFace = {}
 AudioFace.music = {}
